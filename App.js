050c3bad4ddf5a55b121e14b682b165a1e51944f
omprakash524 <omprakashpandit524@gmail.com>
2024-08-09 18:24:01 +0530

    read commit

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading4 = React.createElement("h1", {}, "Hello i am from react and react dom and first react program");
+const root4 = ReactDOM.createRoot(document.getElementById("root"));
+root4.render(heading4);
+
+// const heading =  React.createElement("h1", {},"hello i am om");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
