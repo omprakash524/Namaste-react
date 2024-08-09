7e953d94649381a522b8cecab82f42c34bc39bad
omprakash524 <omprakashpandit524@gmail.com>
2024-08-09 18:45:59 +0530

    react brower

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-const heading4 = React.createElement("h1", {}, "Hello i am from react and react dom and first react program");
+const heading4 = React.createElement("h1", {id:"heading", xyz:"head"}, "Hello i am from react and react dom and first react program");
+console.log(heading4); // return object
 const root4 = ReactDOM.createRoot(document.getElementById("root"));
 root4.render(heading4);
 
 // const heading =  React.createElement("h1", {},"hello i am om");
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
+
